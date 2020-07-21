4b671e1aebb2b9448f08d6c66136cd637204a92f
Patrick Doyle <patrickdoyle@Patricks-MacBook-Pro.local>
2020-07-21 18:58:45 -0500

    Initializes the application server.

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs')
+const mongoose = require("mongoose");
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+mongoose.connect("mongodb://localhost:27017/runDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+
+
+
+
+app.get("/", function(req, res){
+  res.render("home")
+});
+
+
+
+
+app.listen(3000, console.log('server is running on port 3000'));
